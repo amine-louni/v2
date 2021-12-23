c8ba83ff7e37264de08a3f3fdbda02ed10d12c9a
amine louni <amine.louni.dev@gmail.com>
2021-12-23 12:09:51 +0100

    design update

--- components/sections/About.jsx
+++ components/sections/About.jsx
@@ -1,4 +1,8 @@
+import Image from "next/image";
 import classes from "../../styles/Home.module.scss";
+import squareBlue from "./../../public/square-blue.svg";
+import zigZagBlue from "./../../public/zigzag-blue.svg";
+import circlesagBlue from "./../../public/circles-blue.svg";
 
 export default function About() {
   return (
@@ -31,9 +35,16 @@ export default function About() {
               <li>TypeScript</li>
               <li>React native</li>
             </ul>
+
+            <figure className={classes["form-2"]}>
+              <Image src={circlesagBlue} alt="zig zag" />
+            </figure>
           </div>
 
-          <div className="col-lg-6 ">
+          <div className={`col-lg-6 ${classes["design-col"]}`}>
+            <figure className={classes["form-1"]}>
+              <Image src={squareBlue} alt="zig zag" />
+            </figure>
             <h3>Passion for design</h3>
             <p>
               Being a Web designer enables you to not only work with websites
