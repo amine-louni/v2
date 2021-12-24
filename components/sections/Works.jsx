65b7345572f1329f8ea6c055af91d8ef722c8e7e
amine louni <amine.louni.dev@gmail.com>
2021-12-24 15:28:11 +0100

    design update

--- components/sections/Works.jsx
+++ components/sections/Works.jsx
@@ -21,68 +21,7 @@ export default function Works() {
             <div className="project">
               <div className="project__content">
                 <div className="project__cta">
-                  <FiFolder size={45} />
-                  <div>
-                    <a href="#" className="project__link">
-                      <FiExternalLink />
-                    </a>
-                    <a href="#" className="project__link">
-                      <FiGithub />
-                    </a>
-                  </div>
-                </div>
-                <h3 className="project__title">Lorem, ipsum dolor.</h3>
-                <div className="project__skills">
-                  <div className="project__skills__tag">Node.js</div>
-                  <div className="project__skills__tag">React</div>
-                  <div className="project__skills__tag">REST API</div>
-                  <div className="project__skills__tag">Postgres</div>
-                </div>
-
-                <p className="project__description">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
-                  itaque enim dolorum, deserunt doloribus dolor nulla
-                  exercitationem perferendis.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-6">
-            <div className="project">
-              <div className="project__content">
-                <div className="project__cta">
-                  <FiFolder size={45} />
-                  <div>
-                    <a href="#" className="project__link">
-                      <FiExternalLink />
-                    </a>
-                    <a href="#" className="project__link">
-                      <FiGithub />
-                    </a>
-                  </div>
-                </div>
-                <h3 className="project__title">Lorem, ipsum dolor.</h3>
-                <div className="project__skills">
-                  <div className="project__skills__tag">Node.js</div>
-                  <div className="project__skills__tag">React</div>
-                  <div className="project__skills__tag">REST API</div>
-                  <div className="project__skills__tag">Postgres</div>
-                </div>
-
-                <p className="project__description">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio
-                  itaque enim dolorum, deserunt doloribus dolor nulla
-                  exercitationem perferendis.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="project">
-              <div className="project__content">
-                <div className="project__cta">
-                  <FiFolder size={45} />
+                  <FiFolder size={45} className="project__icon" />
                   <div>
                     <a href="#" className="project__link">
                       <FiExternalLink />
