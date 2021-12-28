a1e6c4ac8397e50983da9e02e5098784f65dc281
amine louni <amine.louni.dev@gmail.com>
2021-12-28 13:33:37 +0100

    Hero animation

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import { Link } from "react-scroll";
 
+const links = [
+  {
+    title: "about",
+    url: "about",
+  },
+  {
+    title: "jobs",
+    url: "jobs",
+  },
+  {
+    title: "works",
+    url: "works",
+  },
+  {
+    title: "contact",
+    url: "contact",
+  },
+];
+
 const Navbar = ({ open, setOpen }) => {
   return (
     <>
@@ -14,58 +33,31 @@ const Navbar = ({ open, setOpen }) => {
       </div>
       <nav className="root">
         <div className="container">
-          <h4>Amine Louni</h4>
+          <h4 className="animate__animated animate__fadeInDown  animate-delay-1">
+            Amine Louni
+          </h4>
           <ul>
-            <li>
-              <Link
-                activeClass="active"
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
+            {links.map((link, index) => (
+              <li
+                className={`animate__animated animate__fadeInDown  animate-delay-${
+                  index + 1
+                }`}
+                key={link.title}
               >
-                <span>01.</span> about
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="active"
-                to="jobs"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <span>02.</span> experience
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="active"
-                to="works"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <span>03.</span> works
-              </Link>
-            </li>
-            <li>
-              <Link
-                activeClass="active"
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <span>04.</span> contact
-              </Link>
-            </li>
+                <Link
+                  activeClass="active"
+                  to={link.url}
+                  spy={true}
+                  smooth={true}
+                  offset={-70}
+                  duration={500}
+                >
+                  <span>0{index + 1}.</span> {link.title}
+                </Link>
+              </li>
+            ))}
 
-            <li>
+            <li className="animate__animated animate__fadeInDown  animate-delay-7">
               <button className="btn btn-sm">Resume</button>
             </li>
           </ul>
--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -4,36 +4,56 @@ import AvatarShapes from "../../public/photo-bg.svg";
 export default function Hero() {
   return (
     <section className="hero">
-      <div className="overlay" />
+      <div className="overlay animate__animated animate__fadeInDown  animate-delay-7" />
       <div className={`container`}>
         <div className={`row  `}>
           <div className={`col-lg-7  main-col `}>
             <div>
-              <h4 className={"hello"}>
+              <h4
+                className={
+                  "hello animate__animated animate__fadeInUp  animate-delay-10"
+                }
+              >
                 <span>▶</span> Hy my name is Amine Louni
               </h4>
-              <h4 className={"title"}>
+              <h4
+                className={
+                  "title animate__animated animate__fadeIn  animate-delay-15"
+                }
+              >
                 Full Stack <br /> Developer
                 <span className={"titlePart"}>.</span>
               </h4>
-              <p className={"text"}>
+              <p
+                className={
+                  "text animate__animated animate__fadeIn  animate-delay-15"
+                }
+              >
                 Since beginning my journey as a software engineer , I &apos;ve
                 done remote work for agencies, consulted for startups, and
                 collaborated with talented people to create digital products for
                 both business and consumer use.
               </p>
-              <button className="btn">
+              <button className="btn animate__animated animate__fadeInUp  animate-delay-20">
                 <span className="btn__text">Check out my works</span>{" "}
               </button>
               <br />
             </div>
           </div>
           <div className={`col-lg-5  col-photo`}>
-            <div className={"xs-overlay"} />
-            <div className="avatarShapes">
+            <div
+              className={
+                "xs-overlay animate__animated animate__fadeIn  animate-delay-7"
+              }
+            />
+            <div className="avatarShapes animate__animated animate__fadeIn  animate-delay-7">
               <Image src={AvatarShapes} alt="shapes" />
             </div>
-            <div className={"photo"}></div>
+            <div
+              className={
+                "photo animate__animated animate__fadeIn  animate-delay-7"
+              }
+            ></div>
           </div>
         </div>
       </div>
